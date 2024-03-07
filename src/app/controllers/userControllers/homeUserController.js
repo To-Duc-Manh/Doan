7560c3647a061aea48e8788425c4d649f6d4255d
To-Duc-Manh <manhto150601@gmail.com>
2024-03-07 18:06:23 +0700

    nuw

--- src/app/controllers/userControllers/homeUserController.js
+++ src/app/controllers/userControllers/homeUserController.js
@@ -159,16 +159,16 @@ class homeUserController {
             // Kết quả trả về từ stored procedure
             let products_datHang = results[0]; // Lấy danh sách thông tin chi tiết sản phẩm
 
-            // res.render('user/dat_hang.ejs', {
-            //     user: req.session.user,
-            //     products_datHang: products_datHang,
-            //     so_luong: so_luong + 1
-            // });
-            console.log('p', products_datHang);
+            res.render('user/mm.ejs', {
+                user: req.session.user,
+                products_datHang: products_datHang,
+                so_luong: so_luong + 1
+            });
         });
         console.log('selectedProducts', selectedProducts);
 
     }
+    
     dat_hang2(req, res) {
         const { hoTen, soDienThoai, diaChi, ghiChu, hinhThucThanhToan, soLuong, giaBan, tongGia, tong_tien_thanh_toan, san_pham_id } = req.body;
 
